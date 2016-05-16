7a63a484833905fdcbc3c930a461c31c83951217
alxia <aldridgexia@gmail.com>
2016-05-17 00:35:13 +0800

    data handle Update.

--- dataHandle.py
+++ dataHandle.py
@@ -5,6 +5,7 @@ from pandas import DataFrame, Series
 import xlrd
 pd.set_option('expand_frame_repr', False)
 
+#读入数据
 data_path = '/Users/Aldridge/sleepinfo/sleepData.xlsx'
 data_excel = pd.ExcelFile(data_path)
 table = data_excel.parse(u'工作表1')
@@ -14,29 +15,41 @@ new_table['sleep_duration']=new_table['wake_time']-new_table['bed_time'].shift(1
 new_table.index = new_table['date']
 del new_table['date']
 print new_table.tail(7)
-t = new_table.ix['2016/5']['wake_time']
-print t, type(t)
+# t = new_table.ix['2016/5/1']['wake_time']
+# print t, type(t)
 
-mt_time_total = new_table['mt_time'].sum()
-print 'total mt times: %d' % (mt_time_total)
-mt_feb = new_table.ix['2016/2']['mt_time'].sum()
-print 'total mt times in February: %d' % (mt_feb)
-mt_mar = new_table.ix['2016/3']['mt_time'].sum()
-print 'total mt times in March: %d' % (mt_mar)
-mt_apr = new_table.ix['2016/4']['mt_time'].sum()
-print 'total mt times in April: %d' % (mt_apr)
-mt_may = new_table.ix['2016/5']['mt_time'].sum()
-print 'total mt times in May: %d' % (mt_may)
+#处理数据
+month_dict = {1:'January',2:'February',3:'March',4:'April',5:'May',\
+			  6:'June',7:'July',8:'August',9:'September',10:'October',\
+			  11:'November',12:'December'}
+
+def print_mt(table,month=0):
+	if month == 0:
+		mt_total = table['mt_time'].sum()
+		print 'total mt times: %d' % (mt_total)
+	else:
+		given_month = '2016/' + str(month)
+		abrv_month = month_dict[month]
+		mt_month = table.ix[given_month]['mt_time'].sum()
+		print 'total mt times in %s: %d' % (abrv_month,mt_month)
+
+print_mt(new_table)
+for i in range(2,6):
+	print_mt(new_table, i)
 
 def hour_minute(td):
     return (td.seconds//3600, (td.seconds//60)%60)
-sd_avg = new_table['sleep_duration'].mean()
-print 'average sleep duration in all time: %dhrs %dmins' % hour_minute(sd_avg)
-sd_feb = new_table.ix['2016/2']['sleep_duration'].mean()
-print 'average sleep duration in February: %dhrs %dmins' % hour_minute(sd_feb)
-sd_mar = new_table.ix['2016/3']['sleep_duration'].mean()
-print 'average sleep duration in March: %dhrs %dmins' % hour_minute(sd_mar)
-sd_apr = new_table.ix['2016/4']['sleep_duration'].mean()
-print 'average sleep duration in April: %dhrs %dmins' % hour_minute(sd_apr)
-sd_may = new_table.ix['2016/5']['sleep_duration'].mean()
-print 'average sleep duration in May: %dhrs %dmins' % hour_minute(sd_may)
+
+def print_sd(table,month=0):
+	if month == 0:
+		sd_avg = table['sleep_duration'].mean()
+		print 'average sleep duration: %dhrs %dmins' % hour_minute(sd_avg)
+	else:
+		given_month = '2016/' + str(month)
+		abrv_month = month_dict[month]
+		sd_month = table.ix[given_month]['sleep_duration'].mean()
+		print 'average sleep duration in %s: %dhrs %dmins' % ((abrv_month,)+hour_minute(sd_month))
+
+print_sd(new_table)
+for i in range(2,6):
+	print_sd(new_table, i)
